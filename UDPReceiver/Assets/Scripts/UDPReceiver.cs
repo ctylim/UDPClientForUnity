7da3b69ab9d2c147843d4dd83688f22e33e8cf68
ctylim <lim.0.ctyl@gmail.com>
2017-05-05 23:10:26 +0900

    UDP Receiver

---
+++ UDPReceiver/Assets/Scripts/UDPReceiver.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using UniRx;
+
+// Reference: http://qiita.com/Napier_271828_/items/0d489c7d833047241079
+public class UdpState : System.IEquatable<UdpState> {
+    //UDP通信の情報を収める。送受信ともに使える
+    public IPEndPoint EndPoint {
+        get; set;
+    }
+    public string UdpMsg {
+        get; set;
+    }
+
+    public UdpState(IPEndPoint ep, string udpMsg) {
+        this.EndPoint = ep;
+        this.UdpMsg = udpMsg;
+    }
+    public override int GetHashCode() {
+        return EndPoint.Address.GetHashCode();
+    }
+
+    public bool Equals(UdpState s) {
+        if (s == null) {
+            return false;
+        }
+        return EndPoint.Address.Equals(s.EndPoint.Address);
+    }
+}
+
+public class UDPReceiver : MonoBehaviour {
+    private const int listenPort = 2222;
+    private static UdpClient udpClient;
+    private bool isReceiving = false;
+    private bool isApplicationQuit = false;
+    public IObservable<UdpState> _udpSequence;
+
+    void Awake() {
+        _udpSequence = Observable.Create<UdpState>(observer => {
+            Debug.Log(string.Format("_udpSequence thread: {0}", System.Threading.Thread.CurrentThread.ManagedThreadId));
+            try {
+                udpClient = new UdpClient(listenPort);
+            }
+            catch (SocketException ex) {
+                observer.OnError(ex);
+            }
+            IPEndPoint remoteEP = null;
+            udpClient.EnableBroadcast = true;
+            while (!isApplicationQuit) {
+                try {
+                    remoteEP = null;
+                    var receivedMsg = System.Text.Encoding.ASCII.GetString(udpClient.Receive(ref remoteEP));
+                    observer.OnNext(new UdpState(remoteEP, receivedMsg));
+                }
+                catch (SocketException) {
+                    Debug.Log("UDP::Receive timeout");
+                }
+            }
+            observer.OnCompleted();
+            return Disposable.Empty;
+        })
+        .Where(_ => isReceiving)
+        .SubscribeOn(Scheduler.ThreadPool)
+        .Publish()
+        .RefCount();
+    }
+
+    public void StartSubscribe() {
+        isReceiving = true;
+    }
+
+    public void StopSubscribe() {
+        isReceiving = false;
+    }
+
+    void OnApplicationQuit() {
+        isApplicationQuit = true;
+        udpClient.Client.Blocking = false;
+    }
+}
--- UDPReceiver/Assets/Scripts/UDPReceiverManager.cs
+++ UDPReceiver/Assets/Scripts/UDPReceiverManager.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
 
 public class UDPReceiverManager : MonoBehaviour {
 
-	// Use this for initialization
+    public UDPReceiver udpReceiver;
+    IObservable<UdpState> udpSequence;
+    public Text ReceiveText;
+
 	void Start () {
-		
+        udpSequence = udpReceiver._udpSequence;
+        udpSequence.ObserveOnMainThread()
+                   .Subscribe(x => {
+                       Debug.Log(x.UdpMsg);
+                       ReceiveText.text = x.UdpMsg;
+                   })
+                   .AddTo(this);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void StartSubscribeButtonClick() {
+        udpReceiver.StartSubscribe();
+    }
+
+    public void StopSubscribeButtonClick() {
+        udpReceiver.StopSubscribe();
+    }
+
 }
